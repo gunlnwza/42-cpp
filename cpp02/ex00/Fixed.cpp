e7e376bfc40998736c4d1529307d2485fdbdec78
Natachat Teecharcharn <nteechar@Natachats-MacBook-Air.local>
2025-01-19 23:03:58 +0700

    cpp02/ex00: Done

--- cpp02/ex00/Fixed.cpp
+++ cpp02/ex00/Fixed.cpp
@@ -1,24 +1,25 @@
-#include <iostream>
 #include "Fixed.hpp"
+
+// ---- ex00 -------------------------------------------------------------------
 
 const int Fixed::nb_fractional_bits = 8;
 
 Fixed::Fixed(void)
-    : raw(0)
 {
     std::cerr << "Default constructor called" << std::endl;
+    this->raw = 0;
 }
 
 Fixed::Fixed(const Fixed &other)
-    : raw(other.raw)
 {
     std::cerr << "Copy constructor called" << std::endl;
+    *this = other;
 }
 
 Fixed& Fixed::operator=(const Fixed &other)
 {
     std::cerr << "Copy assignment operator called" << std::endl;
-    this->raw = other.raw;
+    this->raw = other.getRawBits();
     return (*this);
 }
 
@@ -38,3 +39,5 @@ void Fixed::setRawBits(int const raw)
     std::cerr << "setRawBits member function called" << std::endl;
     this->raw = raw;
 }
+
+// -----------------------------------------------------------------------------
--- cpp02/ex00/Fixed.hpp
+++ cpp02/ex00/Fixed.hpp
@@ -1,9 +1,12 @@
 #ifndef FIXED_HPP
 # define FIXED_HPP
 
+# include <iostream>
+
 class Fixed
 {
     public:
+        // ---- ex00 -----------------------------------------------------------
         Fixed(void);
         Fixed(const Fixed &other);
         Fixed& operator=(const Fixed &other);
@@ -11,6 +14,7 @@ class Fixed
 
         int  getRawBits(void) const;
         void setRawBits(int const raw);
+        // ---------------------------------------------------------------------
 
     private:
         int              raw;
