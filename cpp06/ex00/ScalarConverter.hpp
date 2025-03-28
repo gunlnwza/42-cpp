2a1c10882ad75ada07ffcf9fa78f1bc1fca29600
Natachat Teecharcharn <nteechar@Natachats-MacBook-Air.local>
2025-03-28 23:29:36 +0700

    Save work (cpp06/ex00)

--- cpp06/ex00/ScalarConverter.hpp
+++ cpp06/ex00/ScalarConverter.hpp
@@ -1,11 +1,6 @@
 #ifndef SCALAR_CONVERTER_HPP
 # define SCALAR_CONVERTER_HPP
 
-# include <cstdlib>
-# include <cmath>
-# include <cerrno>
-# include <cstring>
-# include <limits>
 # include <string>
 # include <iostream>
 
@@ -17,6 +12,26 @@ typedef enum e_scalar_type
 	TYPE_DOUBLE,
 	TYPE_INVALID
 }	t_scalar_type;
+
+typedef enum e_issue
+{
+	OK,
+	ISSUE_OVERFLOW,
+	ISSUE_UNDERFLOW,
+}	t_issue;
+
+typedef struct s_scalar_field
+{
+	char	c;
+	int		i;
+	float	f;
+	double	d;
+
+	t_issue	c_issue;
+	t_issue	i_issue;
+	t_issue	f_issue;
+	t_issue	d_issue;
+}	t_scalar_field;
 
 class ScalarConverter
 {
