7dcf35893a46377cea9a2f601bd91d5d68fa788d
Natachat Teecharcharn <nteechar@Natachats-MacBook-Air.local>
2025-03-29 14:52:57 +0700

    Most thing ok, clean up later

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -40,7 +40,7 @@ t_scalar_type	identify_type(const std::string& input)
 	i = std::strtol(ptr, &ptr, 10);
 	if (*ptr == '\0')
 		return (TYPE_DOUBLE);
-	else if (*ptr == 'f' && *(ptr + 1) == '\0')
+	else if (*ptr == 'f' && ptr[1] == '\0')
 		return (TYPE_FLOAT);
 	
 	(void) i;
@@ -54,15 +54,24 @@ void	convert_char(const std::string &input, t_scalar_field &field)
 	field.i = static_cast<int>(field.c);
 	field.f = static_cast<float>(field.c);
 	field.d = static_cast<double>(field.c);
+
+	if (std::isprint(field.c))
+		std::cout << "char: " << field.c << std::endl;
+	else if (field.c < 0)
+		std::cout << "char: " << "no character for negative value" << std::endl;
+	else
+		std::cout << "char: " << "Non displayable" << std::endl;
+	std::cout << "int: " << field.i << std::endl;
+	std::cout << "float: " << field.f << std::endl;
+	std::cout << "double: " << field.d << std::endl;
 }
 
 void	convert_int(const std::string &input, t_scalar_field &field)
 {
 	long	value;
-	char	*ptr;
 
 	errno = 0;
-	value = std::strtol(input.c_str(), &ptr, 10);
+	value = std::strtol(input.c_str(), NULL, 10);
 	if (value < std::numeric_limits<int>::lowest())
 	{
 		std::cout << "char: " << "value dependent on int" << std::endl;
@@ -102,28 +111,107 @@ void	convert_int(const std::string &input, t_scalar_field &field)
 
 void	convert_float(const std::string &input, t_scalar_field &field)
 {
-	field.f = std::atof(input.c_str());
+	float	value;
 
-	field.c = static_cast<char>(field.f);
+	errno = 0;
+	value = std::strtof(input.c_str(), NULL);
+	if (errno == ERANGE)
+	{
+		if (value > std::numeric_limits<float>::max())
+		{
+			std::cout << "char: " << "value dependent on float" << std::endl;
+			std::cout << "int: " << "value dependent on float" << std::endl;
+			std::cout << "float: " << "overflow" << std::endl;
+			std::cout << "double: " << "value dependent on float" << std::endl;
+		}
+		else if (value < std::numeric_limits<float>::lowest())
+		{
+			std::cout << "char: " << "value dependent on float" << std::endl;
+			std::cout << "int: " << "value dependent on float" << std::endl;
+			std::cout << "float: " << "underflow" << std::endl;
+			std::cout << "double: " << "value dependent on float" << std::endl;
+		}
+		return ;
+	}
+	field.f = value;
+
 	field.i = static_cast<int>(field.f);
+	field.c = static_cast<char>(field.f);
 	field.d = static_cast<double>(field.f);
+
+	if (field.f < std::numeric_limits<char>::lowest())
+		std::cout << "char: " << "underflow" << std::endl;
+	else if (std::numeric_limits<char>::max() < field.f)
+		std::cout << "char: " << "overflow" << std::endl;
+	else if (std::isprint(field.c))
+		std::cout << "char: " << field.c << std::endl;
+	else if (field.c < 0)
+		std::cout << "char: " << "no character for negative value" << std::endl;
+	else
+		std::cout << "char: " << "Non displayable" << std::endl;
+	if (field.f < std::numeric_limits<int>::lowest())
+		std::cout << "int: " << "underflow" << std::endl;
+	else if (field.f > std::numeric_limits<int>::max())
+		std::cout << "int: " << "overflow" << std::endl;
+	else
+		std::cout << "int: " << field.i << std::endl;
+	std::cout << "float: " << field.f << std::endl;
+	std::cout << "double: " << field.d << std::endl;
 }
 
 void	convert_double(const std::string &input, t_scalar_field &field)
 {
-	field.d = std::atof(input.c_str());
-	
-	field.c = static_cast<char>(field.d);
-	field.i = static_cast<int>(field.d);
-	field.f = static_cast<float>(field.d);
-}
+	double	value;
 
-void	print_result(t_scalar_field &field)
-{
-	std::cout << "char: " << field.c << std::endl
-			  << "int: " << field.i << std::endl
-			  << "float: " << field.f << std::endl
-			  << "double: " << field.d << std::endl;
+	errno = 0;
+	value = std::strtod(input.c_str(), NULL);
+	if (errno == ERANGE)
+	{
+		if (value > std::numeric_limits<double>::max())
+		{
+			std::cout << "char: " << "value dependent on double" << std::endl;
+			std::cout << "int: " << "value dependent on double" << std::endl;
+			std::cout << "float: " << "value dependent on double" << std::endl;
+			std::cout << "double: " << "overflow" << std::endl;
+		}
+		else if (value < std::numeric_limits<double>::lowest())
+		{
+			std::cout << "char: " << "value dependent on double" << std::endl;
+			std::cout << "int: " << "value dependent on double" << std::endl;
+			std::cout << "float: " << "value dependent on double" << std::endl;
+			std::cout << "double: " << "underflow" << std::endl;
+		}
+		return ;
+	}
+	field.d = value;
+
+	field.i = static_cast<int>(field.d);
+	field.c = static_cast<char>(field.d);
+	field.f = static_cast<float>(field.d);
+
+	if (field.d < std::numeric_limits<char>::lowest())
+		std::cout << "char: " << "underflow" << std::endl;
+	else if (std::numeric_limits<char>::max() < field.d)
+		std::cout << "char: " << "overflow" << std::endl;
+	else if (std::isprint(field.c))
+		std::cout << "char: " << field.c << std::endl;
+	else if (field.c < 0)
+		std::cout << "char: " << "no character for negative value" << std::endl;
+	else
+		std::cout << "char: " << "Non displayable" << std::endl;
+	if (field.d < std::numeric_limits<int>::lowest())
+		std::cout << "int: " << "underflow" << std::endl;
+	else if (field.d > std::numeric_limits<int>::max())
+		std::cout << "int: " << "overflow" << std::endl;
+	else
+		std::cout << "int: " << field.i << std::endl;
+	if (field.d < std::numeric_limits<float>::lowest())
+		std::cout << "float: " << "underflow" << std::endl;
+	else if (field.d > std::numeric_limits<float>::max())
+		std::cout << "float: " << "overflow" << std::endl;
+	else
+		std::cout << "float: " << field.f << std::endl;
+	std::cout << "double: " << field.d << std::endl;
 }
 
 void	ScalarConverter::convert(const std::string& input)
