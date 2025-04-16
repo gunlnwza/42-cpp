b050680a40be7ecbef0a58c9dd7e20f8aa05c761
Natachat Teecharcharn <nteechar@Natachats-MacBook-Air.local>
2025-04-16 23:01:21 +0700

    Do cpp08/ex00

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -14,17 +14,4 @@ typename T::iterator easyfind(T& container, int value)
 	return (it);
 }
 
-// template <typename T>
-// typename T::iterator easyfind(const T& arr, int value)
-// {
-// 	size_t					len;
-// 	typename T::iterator	it;
-
-// 	len = sizeof(arr) / sizeof(arr[0]);
-// 	it = std::find(arr, arr + len, value);
-// 	if (it != arr + len)
-// 		throw std::runtime_error("Value not found in container");
-// 	return (it);
-// }
-
 #endif
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -1,19 +1,88 @@
 #include <iostream>
+
 #include <vector>
+#include <array>
+#include <list>
+#include <deque>
+#include <forward_list>
+#include <set>
+#include <unordered_set>
 
 #include "easyfind.hpp"
 
-int main(void)
+int f(int x)
 {
-	std::vector<int> myvector;
-	myvector.push_back(1);
-	myvector.push_back(2);
+	return ((x + 1) * 10);
+}
 
-	std::cout << myvector[0] << std::endl;
-	std::cout << myvector[1] << std::endl;
-	std::cout << myvector[2] << std::endl;
+template <typename T>
+void	try_to_find(const char* container_name, T& container, int item)
+{
+	typename T::iterator it;
+	
+	// std::cout << container_name << ": "; 
+	// it = std::find(container.begin(), container.end(), item);
+	// if (it != container.end())
+	// 	std::cout << "FOUND! (*it = " << *it << ")" << std::endl;
+	// else
+	// 	std::cout << "Not found..." << std::endl;
 
-	// std::cout << easyfind(myvector, 3) << std::endl;
+	std::cout << container_name << ": ";
+	try
+	{
+		it = easyfind(container, item);
+		std::cout << "FOUND! (*it = " << *it << ")" << std::endl;
+	}
+	catch (std::runtime_error& e)
+	{
+		std::cout << "Not found..." << std::endl;
+	}
+}
 
-	return 0;
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		std::cout << "Usage: ./easyfind <item>" << std::endl;
+		return (1);
+	}
+	const int	N = 4;
+	const int	item = std::atoi(argv[1]);
+
+	std::vector<int> vector;
+	for (int i = 0; i < N; i++)
+		vector.push_back(f(i));
+	try_to_find("vector", vector, item);
+
+	std::array<int, N> array;
+	for (int i = 0; i < N; i++)
+		array[i] = f(i);
+	try_to_find("array", array, item);
+
+	std::list<int> list;
+	for (int i = 0; i < N; i++)
+		list.push_back(f(i));
+	try_to_find("list", list, item);
+
+	std::deque<int> deque;
+	for (int i = 0; i < N; i++)
+		deque.push_back(f(i));
+	try_to_find("deque", deque, item);
+
+	std::forward_list<int> forward_list;
+	for (int i = 0; i < N; i++)
+		forward_list.push_front(f(i));
+	try_to_find("forward_list", forward_list, item);
+
+	std::set<int> set;
+	for (int i = 0; i < N; i++)
+		set.insert(f(i));
+	try_to_find("set", set, item);
+
+	std::unordered_set<int> unordered_set;
+	for (int i = 0; i < N; i++)
+		unordered_set.insert(f(i));
+	try_to_find("unordered_set", set, item);
+
+	return (0);
 }
