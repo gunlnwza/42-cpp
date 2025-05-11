aa4b6797de0b00bce5d1d7085668072b40e4a7d8
Natachat Teecharcharn <nteechar@Natachats-MacBook-Air.local>
2025-05-11 16:02:01 +0700

    Do cpp09/ex01

--- cpp09/ex01/RPN.cpp
+++ cpp09/ex01/RPN.cpp
@@ -54,7 +54,6 @@ void RPN::tokenize(const std::string& expression)
     }
 }
 
-
 static long calculate_operation(long a, char op, long b)
 {
     switch (op)
@@ -62,7 +61,10 @@ static long calculate_operation(long a, char op, long b)
         case '+': return (a + b);
         case '-': return (a - b);
         case '*': return (a * b);
-        case '/': return (a / b);
+        case '/':
+            if (b == 0)
+                throw (std::runtime_error("process_current_token: division by 0"));
+            return (a / b);
         default: throw (std::runtime_error("process_current_token: unknown operator"));
     }
 }
@@ -70,8 +72,8 @@ static long calculate_operation(long a, char op, long b)
 void RPN::process_current_token()
 {
     char    token;
-    long    a;
-    long    b;
+    long    left;
+    long    right;
     long    result;
 
     token = pop(tokens);
@@ -81,11 +83,11 @@ void RPN::process_current_token()
     {
         if (stack.empty())
             throw (std::runtime_error("process_current_token: no num in stack for left num"));
-        a = pop(stack);
+        right = pop(stack);
         if (stack.empty())
             throw (std::runtime_error("process_current_token: no num in stack for right num"));
-        b = pop(stack);
-        result = calculate_operation(a, token, b);
+        left = pop(stack);
+        result = calculate_operation(left, token, right);
         stack.push(result);
     }
 }
@@ -101,6 +103,6 @@ void RPN::evaluate(const std::string& expression)
         throw (std::runtime_error("evaluate: no result num"));
     result = pop(stack);
     if (!stack.empty())
-        throw (std::runtime_error("evaluate: result nums more than one"));
+        throw (std::runtime_error("evaluate: more than 1 result nums"));
     std::cout << result << std::endl;
 }
