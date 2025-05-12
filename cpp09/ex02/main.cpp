cc5216edda6b70807712e5a8217203b6a9c3e547
Natachat Teecharcharn <nteechar@Natachats-MacBook-Air.local>
2025-05-12 16:26:17 +0700

    Read about FJ, hard

--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -3,6 +3,10 @@
 #include "VectorSorter.hpp"
 #include "DequeSorter.hpp"
 
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define RESET "\033[0m"
+
 // Containers used: std::vector, std::deque
 
 static void parse_inputs(std::vector<int>& inputs, int argc, char **argv)
@@ -58,21 +62,21 @@ int	main(int argc, char **argv)
     vector_sorter.selection_sort();
     vector_sorter.time_stop();
 
-    deque_sorter.time_start();
-    deque_sorter.read_inputs(inputs);
+    // deque_sorter.time_start();
+    // deque_sorter.read_inputs(inputs);
     // deque_sorter.merge_insertion_sort();
-    deque_sorter.selection_sort();
-    deque_sorter.time_stop();
+    // deque_sorter.selection_sort();
+    // deque_sorter.time_stop();
 
     std::cout << "Before: " << inputs << std::endl;
     std::cout << "After:  " << vector_sorter.get_vector() << std::endl;
     std::cout << "Time to process a range of " << N << " elements with std::vector : " << vector_sorter.get_microseconds() << " microseconds" << std::endl;
-    std::cout << "Time to process a range of " << N << " elements with std::deque : " << deque_sorter.get_microseconds() << " microseconds" << std::endl;
+    // std::cout << "Time to process a range of " << N << " elements with std::deque : " << deque_sorter.get_microseconds() << " microseconds" << std::endl;
 
     std::cout << "[ DEBUG ]" << std::endl;
-    std::cout << "vector's compare count: " << vector_sorter.get_compare_count() << std::endl;
-    std::cout << "deque's compare count: " << deque_sorter.get_compare_count() << std::endl;
-    std::cout << "theoretical bound: " << F(N) << std::endl;
+    std::cout << "Vector's compare count: " << vector_sorter.get_compare_count() << std::endl;
+    // std::cout << "deque's compare count: " << deque_sorter.get_compare_count() << std::endl;
+    std::cout << "Theoretical bound: " << (vector_sorter.get_compare_count() <= F(N) ? GREEN : RED) << F(N) << RESET << std::endl;
 
     return (EXIT_SUCCESS);
 }
