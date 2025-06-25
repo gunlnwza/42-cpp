66a1f4ab7a412855fa0d228b823416b5e70467fb
Natachat Teecharcharn <nteechar@Natachats-MacBook-Air.local>
2025-06-25 21:15:53 +0700

    main.cpp is good

--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -7,11 +7,14 @@ int	main(int argc, char **argv)
 	if (argc != 2)
 	{
 		std::cout << "Error: Wrong argument format" << std::endl;
-		std::cout << "Usage: ./btc <query_filename>" << std::endl;
+		std::cout << "Usage: ./btc <query_file_name>" << std::endl;
 		return (EXIT_FAILURE);
 	}
 
-	BitcoinExchange	calculator("database/data.csv");
+	BitcoinExchange	calculator("database/data_small.csv");
+	// BitcoinExchange	calculator("database/data.csv");
+
 	calculator.evaluate_query(argv[1]);
+
 	return (EXIT_SUCCESS);
 }
